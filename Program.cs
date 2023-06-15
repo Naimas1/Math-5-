ee8065c176ad7e58dd90b4d0470889b701a944f7
Naimas <Naimas@DESKTOP-68Q4MHG>
2023-06-15 19:58:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть перше число:");
+        int number1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введіть друге число:");
+        int number2 = int.Parse(Console.ReadLine());
+        int start = Math.Min(number1, number2);
+        int end = Math.Max(number1, number2);
+
+        Console.WriteLine("Парні числа у вказаному діапазоні:");
+
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
